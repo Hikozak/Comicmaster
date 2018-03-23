1f5279d2413def2766b62752c072fa9b1cc6db91
edge24 <emmanuel.emit@gmail.com>
2018-03-23 22:15:47 +0800

    drag-and-drop added

---
+++ js/DragAndDrop.js
@@ -0,0 +1,31 @@
+var DragAndDrop = (function(){
+    var galleryUploader;
+    
+    function init(){
+        console.log("drag-and-drop ready!");
+        
+        galleryUploader = new qq.FineUploader({
+            element: document.getElementById("fine-uploader-gallery"),
+            template: 'qq-template-gallery',
+            request: {
+                endpoint: '/server/uploads'
+            },
+            thumbnails: {
+                placeholders: {
+                    waitingPath: '/js/placeholders/waiting-generic.png',
+                    notAvailablePath: '/js/placeholders/not_available-generic.png'
+                }
+            },
+            validation: {
+                allowedExtensions: ['jpeg', 'jpg', 'gif', 'png']
+            }
+        });
+    }
+    
+    
+    var public_api = {
+        init: init
+    };
+    
+    return public_api;
+})();
